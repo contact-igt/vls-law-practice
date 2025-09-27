f6b0ca61632b2e6c82e753e7583bded26b0693e3
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-09-27 18:26:24 +0530

    added gtm code

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -31,8 +31,27 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
         />
+        {/* Google Tag Manager */}
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5BDVR8X9');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5BDVR8X9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Main />
         <NextScript />
         <script
